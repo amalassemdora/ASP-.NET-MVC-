be96091cbe18cff4e8ffed9502d323560db7514b
Amal Assem <amalassem21@gmail.com>
2021-07-01 23:53:21 +0200

    Initial Commit

---
+++ Day2Task/Day2Task/Controllers/CourseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day2Task.Models;
+namespace Day2Task.Controllers
+{
+    public class CourseController : Controller
+    {
+        ITIContext db = new ITIContext();
+        public ViewResult Index()
+        {
+            List<Course> crs = db.Courses.ToList(); 
+            return View(crs);
+        }
+        public ActionResult create()
+		{
+            List<Topic> tops = db.Topics.ToList();
+            ViewBag.tops = tops;
+            return View();
+		}
+        public ActionResult Save(Course c)
+		{
+            db.Courses.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("index");
+		}
+        public ActionResult delete(int id)
+		{
+            Course c = db.Courses.Where(n => n.Crs_Id == id).SingleOrDefault();
+            db.Courses.Remove(c);
+            db.SaveChanges();
+            return RedirectToAction("index");
+		}
+    }
+}
---
+++ Day2Task/Day2Task/Controllers/OperationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2Task.Controllers
+{
+    public class OperationController : Controller
+    {
+        // GET: Operation
+        public ViewResult register()
+		{
+            return View();
+		}
+        public ViewResult show(int id,string name,int age)
+		{
+            return View();
+		}
+    }
+}
